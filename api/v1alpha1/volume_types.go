d40bf89c513b58369e8c0a89bbd53ff84e2aa6f0
Petar Cvitanovic <petar@unikraft.io>
2026-01-02 12:02:07 +0100

    feat: Validate CRD fields as immutable
    Signed-off-by: Petar Cvitanovic <petar@unikraft.io>

--- api/v1alpha1/volume_types.go
+++ api/v1alpha1/volume_types.go
@@ -49,6 +49,7 @@ type Volume struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// +kubebuilder:validation:XValidation:rule="(!has(oldSelf.name) && !has(self.name)) || (has(oldSelf.name) && has(self.name) && self.name == oldSelf.name)",message="spec.name is immutable"
 	Spec   platform.CreateVolumeRequest  `json:"spec,omitempty"`
 	Status platform.CreateVolumeResponse `json:"status,omitempty"`
 }
